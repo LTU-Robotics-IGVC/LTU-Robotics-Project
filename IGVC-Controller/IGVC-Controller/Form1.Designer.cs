77db526dc00abd5e5d9885636d2aefc4f499ee9c
cambeyer <cam.beyer@gmail.com>
2015-01-30 15:03:02 -0500

    added FPS count label

--- IGVC-Controller/IGVC-Controller/Form1.Designer.cs
+++ IGVC-Controller/IGVC-Controller/Form1.Designer.cs
@@ -30,36 +30,49 @@
         {
             this.components = new System.ComponentModel.Container();
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.lblFPS = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // imageBox1
             // 
-            this.imageBox1.Location = new System.Drawing.Point(60, 28);
-            this.imageBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.imageBox1.Location = new System.Drawing.Point(45, 23);
+            this.imageBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(640, 480);
+            this.imageBox1.Size = new System.Drawing.Size(480, 390);
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
             // 
+            // lblFPS
+            // 
+            this.lblFPS.AutoSize = true;
+            this.lblFPS.Location = new System.Drawing.Point(45, 5);
+            this.lblFPS.Name = "lblFPS";
+            this.lblFPS.Size = new System.Drawing.Size(27, 13);
+            this.lblFPS.TabIndex = 3;
+            this.lblFPS.Text = "FPS";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(726, 527);
+            this.ClientSize = new System.Drawing.Size(544, 428);
+            this.Controls.Add(this.lblFPS);
             this.Controls.Add(this.imageBox1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Label lblFPS;
     }
 }
 
--- IGVC-Controller/IGVC-Controller/Form1.cs
+++ IGVC-Controller/IGVC-Controller/Form1.cs
@@ -16,12 +16,15 @@ namespace IGVC_Controller
     {
         //This is for testing purposes.  To be removed and organized into stereo vision later.
         Capture capture;
+        int lastGrabbedTicks = -1;
+        int frames;
 
         public Form1()
         {
             InitializeComponent();
             capture = new Capture();
             capture.ImageGrabbed += capture_ImageGrabbed;
+            capture.ImageGrabbed += process_FPS;
             capture.Start();
             this.FormClosing += Form1_FormClosing;
         }
@@ -35,7 +38,23 @@ namespace IGVC_Controller
         void capture_ImageGrabbed(object sender, EventArgs e)
         {
             this.imageBox1.Image = capture.RetrieveBgrFrame().Resize(640, 480, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR).Convert<Gray, byte>();
-            
+        }
+
+        void process_FPS(object sender, EventArgs e)
+        {
+            frames++;
+            int now = Environment.TickCount & Int32.MaxValue;
+            if (lastGrabbedTicks < 0 || now - lastGrabbedTicks < 0)
+            {
+                lastGrabbedTicks = now;
+                frames = 0;
+            }
+            if (now - lastGrabbedTicks > 1000)
+            {
+                this.Invoke(new Action(() => lblFPS.Text = "" + Math.Round(1000 * (frames / (double)(now - lastGrabbedTicks)), 2)));
+                lastGrabbedTicks = now;
+                frames = 0;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
