1d1988402b20592055c683f57c92abe0c00c0d42
Wyharn-Guardian <Wyharn-Guardian@users.noreply.github.com>
2015-02-09 11:29:20 -0500

    Some more moving and added a registry
    Key point is that I added a Registry with abstract examples of how I
    thought i would interface with the modules.  The modules can be self
    contained running by themselves receiving data to be worked on the the
    Registry and sending its finished data  to the Registry.

---
+++ IGVC-Controller/IGVC-Controller/Code/Modules/IModule.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IGVC_Controller.Code.Registries;
+
+namespace IGVC_Controller.Code.Modules
+{
+    /// <summary>
+    /// Module base class that all module types will inherit from
+    /// </summary>
+    class IModule
+    {
+        static private int nextModuleID = 0;
+        protected Registry registry { get; private set; }
+
+        public int moduleID { get; private set; }
+
+        public IModule()
+        {
+            moduleID = nextModuleID;
+        }
+
+        public void bindRegistry(Registry registry) { this.registry = registry; }
+        public void unbindRegistry() { this.registry = null; }
+        public bool isBoundToRegistry() { return this.registry != null; }
+
+        public void recieveDataFromRegistry(string tag, object data);
+
+        public void sendDataToRegistry(string tag, object data)
+        {
+            if(isBoundToRegistry())
+                this.registry.sendData(tag, data);
+        }
+
+        public void sendLogMessageToRegistry(string tag, string message)
+        {
+            if (isBoundToRegistry())
+                this.registry.sendMessageToLogger(tag, message);
+        }
+    }
+}
---
+++ IGVC-Controller/IGVC-Controller/Code/Registries/Registry.cs
@@ -0,0 +1,70 @@
+﻿using IGVC_Controller.Code.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGVC_Controller.Code.Registries
+{
+    class Registry
+    {
+        private List<IModule> interfaceModules;
+        private List<IModule> navigationModules;
+        private List<IModule> mappingModules;
+        private List<IModule> positioningModules;
+        private List<IModule> visionModules;
+
+        private object dataLogger;
+
+        public Registry()
+        {
+            //dataLogger = new DataLogger();
+        }
+
+        public void register(IModule module)
+        {
+            //Maybe we can use types so there only needs to be on register function
+            Type type = module.GetType();
+
+            if(type == typeof(IModule))
+            {
+                
+            }
+
+            //give access to this registry to the module
+            module.bindRegistry(this);
+        }
+
+        public void unbindModule(IModule module)
+        {
+            //remove module from correct list
+
+            //remove access to this registry from the module
+            module.unbindRegistry();
+        }
+
+        public void sendData(string tag, object data)
+        {
+            foreach (IModule module in interfaceModules)
+                module.recieveDataFromRegistry(tag, data);
+
+            foreach (IModule module in navigationModules)
+                module.recieveDataFromRegistry(tag, data);
+
+            foreach (IModule module in mappingModules)
+                module.recieveDataFromRegistry(tag, data);
+
+            foreach (IModule module in positioningModules)
+                module.recieveDataFromRegistry(tag, data);
+
+            foreach (IModule module in visionModules)
+                module.recieveDataFromRegistry(tag, data);
+        }
+
+        public void sendMessageToLogger(string tag, string message)
+        {
+            //this.dataLogger.sendMessage(string tag, string message);
+        }
+    }
+}
