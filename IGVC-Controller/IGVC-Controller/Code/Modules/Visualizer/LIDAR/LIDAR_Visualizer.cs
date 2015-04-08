b30e6640b39f2c093563bb189044506da519a049
Wyharn-Guardian <Wyharn-Guardian@users.noreply.github.com>
2015-04-08 12:48:55 -0400

    Starting to setup a LIDAR data Visualizer
    A Visualizer will open a form when the system is run that it can post
    data to for the user to see.  A similar method can be done if the system
    needs some user inputs.

---
+++ IGVC-Controller/IGVC-Controller/Code/Modules/Visualizer/LIDAR/LIDAR_Visualizer.cs
@@ -0,0 +1,50 @@
+﻿using IGVC_Controller.Code.DataIO;
+using IGVC_Controller.Code.Modules.Visualizer.LIDAR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGVC_Controller.Code.Modules.Visualizer
+{
+    class LIDAR_Visualizer : IModule
+    {
+        GatedVariable LIDARdata = new GatedVariable();
+        LIDAR_Visualizer_Form form;// = new LIDAR_Visualizer_Form();
+
+        public LIDAR_Visualizer()
+        {
+            this.addSubscription(INTERMODULE_VARIABLE.LIDAR_RAW);
+            this.modulePriority = 100;
+        }
+
+        public override void recieveDataFromRegistry(INTERMODULE_VARIABLE tag, object data)
+        {
+            if (tag == INTERMODULE_VARIABLE.LIDAR_RAW)
+                LIDARdata.setObject(data);
+        }
+
+        public override void process()
+        {
+            LIDARdata.shiftObject();
+            this.form.setLIDARData((List<long>)LIDARdata.getObject());
+            base.process();
+        }
+
+        public override bool startup()
+        {
+            form = new LIDAR_Visualizer_Form();
+            form.Show();
+
+            return base.startup();
+        }
+
+        public override void shutdown()
+        {
+            form.Close();
+
+            base.shutdown();
+        }
+    }
+}
---
+++ IGVC-Controller/IGVC-Controller/Code/Modules/Visualizer/LIDAR/LIDAR_Visualizer_Form.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IGVC_Controller.Code.Modules.Visualizer.LIDAR
+{
+    public partial class LIDAR_Visualizer_Form : Form
+    {
+        public LIDAR_Visualizer_Form()
+        {
+            InitializeComponent();
+        }
+
+        public void setLIDARData(List<long> data)
+        {
+
+        }
+    }
+}
