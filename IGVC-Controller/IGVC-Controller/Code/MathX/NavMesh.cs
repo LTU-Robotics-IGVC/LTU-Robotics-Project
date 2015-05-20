89ea5e97864c4edc926481bf7f0c8e8fb618d469
Wyharn-Guardian <Wyharn-Guardian@users.noreply.github.com>
2015-05-20 14:47:46 -0400

    Started work on the NavMesh

---
+++ IGVC-Controller/IGVC-Controller/Code/MathX/NavMesh.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGVC_Controller.Code.MathX
+{
+    class NavMesh
+    {
+        private Node[] nodes;
+
+        private uint unpassable = uint.MaxValue;
+
+        public NavMesh(int width, int height)
+        {
+            //Access is nodes[x + y * width]
+            nodes = new Node[width * height];
+        }
+    }
+
+    struct Node
+    {
+        public Node source;
+        public uint distanceTraveled;
+        public uint distanceRemaining;
+        public uint traverseCost = 1;
+    }
+}
