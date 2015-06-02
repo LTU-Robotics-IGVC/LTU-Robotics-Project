7c832e7c896af56b324273c19dd61ee31cb586b7
Wyharn-Guardian <Wyharn-Guardian@users.noreply.github.com>
2015-06-02 11:50:31 -0400

    An attempt at a generalized viewing window

--- IGVC-Controller/IGVC-Controller/Code/Modules/Visualizer/GeneralVisualizer/GeneralVisualizer.cs
+++ IGVC-Controller/IGVC-Controller/Code/Modules/Visualizer/GeneralVisualizer/GeneralVisualizer.cs
@@ -1,16 +1,85 @@
-﻿using System;
+﻿using IGVC_Controller.Code.DataIO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IGVC_Controller.Code.Modules;
 
 namespace IGVC_Controller.Code.Modules.Visualizer.GeneralVisualizer
 {
     class GeneralVisualizer : IModule
     {
-        public GeneralVisualizer()
+        Dictionary<INTERMODULE_VARIABLE, GatedVariable> channels;
+
+        GeneralVisualizerForm form;
+
+        delegate void delegateCloseForm();
+
+        delegate void delegateSetFormData(INTERMODULE_VARIABLE var, object data);
+        private delegateSetFormData setFormDataDelegate;
+
+        public GeneralVisualizer() : base()
         {
             this.modulePriority = 99;
+            channels = new Dictionary<INTERMODULE_VARIABLE, GatedVariable>();
+            this.addChannel(INTERMODULE_VARIABLE.VISION_LEFT);
+            this.addChannel(INTERMODULE_VARIABLE.VISION_RIGHT);
+            this.addChannel(INTERMODULE_VARIABLE.COLLISION_IMAGE);
+            this.addChannel(INTERMODULE_VARIABLE.LIDAR_RAW);
+            this.addChannel(INTERMODULE_VARIABLE.NAV_MESH);
+            this.addChannel(INTERMODULE_VARIABLE.NAV_PATH);
+        }
+
+        private void addChannel(INTERMODULE_VARIABLE variable)
+        {
+            this.addSubscription(variable);
+            this.channels.Add(variable, new GatedVariable());
+        }
+
+        public override void recieveDataFromRegistry(INTERMODULE_VARIABLE tag, object data)
+        {
+            channels[tag].setObject(data);
+            base.recieveDataFromRegistry(tag, data);
+        }
+
+        public override bool startup()
+        {
+            foreach(INTERMODULE_VARIABLE key in channels.Keys)
+            {
+                channels[key] = new GatedVariable();
+            }
+
+            form = new GeneralVisualizerForm();
+            form.Show();
+            return base.startup();
+        }
+
+        public override void process()
+        {
+            foreach(INTERMODULE_VARIABLE key in channels.Keys)
+            {
+                channels[key].shiftObject();
+                object obj = channels[key].getObject();
+                form.Invoke(this.setFormDataDelegate, new object[] { key, obj });
+            }
+        }
+
+        public override void shutdown()
+        {
+            delegateCloseForm del = this.closeForm;
+            form.Invoke(del, null);
+            base.shutdown();
+        }
+
+        private void closeForm()
+        {
+            form.Close();
+        }
+
+        private void setFormData(INTERMODULE_VARIABLE var, object data)
+        {
+            form.setData(var, data);
         }
     }
 }
--- IGVC-Controller/IGVC-Controller/Code/Modules/Visualizer/GeneralVisualizer/GeneralVisualizerForm.cs
+++ IGVC-Controller/IGVC-Controller/Code/Modules/Visualizer/GeneralVisualizer/GeneralVisualizerForm.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using IGVC_Controller.Code.MathX;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +18,34 @@ namespace IGVC_Controller.Code.Modules.Visualizer.GeneralVisualizer
         public GeneralVisualizerForm()
         {
             InitializeComponent();
+        }
+
+        public void setData(IModule.INTERMODULE_VARIABLE var, object data)
+        {
+            switch(var)
+            {
+                case IModule.INTERMODULE_VARIABLE.VISION_LEFT:
+                    this.imageBox1.Image = (Image<Bgr, byte>)data;
+                    break;
+                case IModule.INTERMODULE_VARIABLE.VISION_RIGHT:
+                    this.imageBox2.Image = (Image<Bgr, byte>)data;
+                    break;
+                case IModule.INTERMODULE_VARIABLE.COLLISION_IMAGE:
+                    this.imageBox3.Image = (Image<Gray, byte>)data;
+                    break;
+                case IModule.INTERMODULE_VARIABLE.NAV_MESH:
+                    NavMesh mesh = (NavMesh)data;
+                    Image<Gray, byte> img = new Image<Gray, byte>(mesh.Width, mesh.Height);
+                    for (int x = 0; x < mesh.Width; x++)
+                        for (int y = 0; y < mesh.Height; y++)
+                        {
+                            img.Data[y, x, 0] = (byte)Math.Min(255, mesh.getNode(x, y).traverseCost);
+                        }
+                    this.imageBox4.Image = img;
+                    break;
+                case IModule.INTERMODULE_VARIABLE.NAV_PATH:
+                    Path path = (Path)data;
+            }
         }
     }
 }
