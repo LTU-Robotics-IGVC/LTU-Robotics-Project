6754cc813a208551164722327acf34e6e3de98b2
Wyharn-Guardian <Wyharn-Guardian@users.noreply.github.com>
2015-06-03 08:50:31 -0400

    Added the decision making stuff

--- IGVC-Controller/IGVC-Controller/Code/Modules/Navigation/SimpleSteering.cs
+++ IGVC-Controller/IGVC-Controller/Code/Modules/Navigation/SimpleSteering.cs
@@ -1,6 +1,8 @@
 ﻿using IGVC_Controller.Code.DataIO;
+using IGVC_Controller.Code.MathX;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +12,99 @@ namespace IGVC_Controller.Code.Modules.Navigation
     class SimpleSteering : IModule
     {
         GatedVariable NavPath;
+        int evalLength = 25;//process the first 25 points
+        double baseSpeed = 0.8;
 
         public SimpleSteering() : base()
         {
             this.modulePriority = 62;
             this.addSubscription(INTERMODULE_VARIABLE.NAV_PATH);
+        }
+
+        public override void recieveDataFromRegistry(INTERMODULE_VARIABLE tag, object data)
+        {
+            switch(tag)
+            {
+                case INTERMODULE_VARIABLE.NAV_PATH:
+                    NavPath.setObject(data);
+                    break;
+            }
+            base.recieveDataFromRegistry(tag, data);
+        }
+
+        public override bool startup()
+        {
+            NavPath = new GatedVariable();
+            return base.startup();
+        }
+
+        public override void process()
+        {
+            NavPath.shiftObject();
+            Path path = (Path)NavPath.getObject();
+            if(path != null)
+            {
+                Point[] points = path.getPointArray();
+                int count = Math.Min(evalLength, points.Length);
+                int runningX = 0;
+                for(int i = 0; i < count; i++)
+                {
+                    runningX += points[i].X;
+                }
+                runningX /= count;
+                double steering = (runningX / (path.MapWidth / 2)) - 1;
+                if (steering < -0.3)
+                    hardLeft();
+                else if (steering < -0.1)
+                    slightLeft();
+                else if (steering < 0.1)
+                    forward();
+                else if (steering < 0.3)
+                    slightRight();
+                else
+                    hardRight();
+            }
+            else
+            {
+                this.sendDataToRegistry(INTERMODULE_VARIABLE.MOTOR_SPEED_LEFT, 0.0);
+                this.sendDataToRegistry(INTERMODULE_VARIABLE.MOTOR_SPEED_RIGHT, 0.0);
+            }
+            base.process();
+        }
+
+        private void slightLeft()
+        {
+            this.sendDataToRegistry(INTERMODULE_VARIABLE.MOTOR_SPEED_LEFT, baseSpeed * 0.5);
+            this.sendDataToRegistry(INTERMODULE_VARIABLE.MOTOR_SPEED_RIGHT, baseSpeed);
+        }
+
+        private void slightRight()
+        {
+            this.sendDataToRegistry(INTERMODULE_VARIABLE.MOTOR_SPEED_LEFT, baseSpeed);
+            this.sendDataToRegistry(INTERMODULE_VARIABLE.MOTOR_SPEED_RIGHT, baseSpeed * 0.5);
+        }
+
+        private void hardLeft()
+        {
+            this.sendDataToRegistry(INTERMODULE_VARIABLE.MOTOR_SPEED_LEFT, 0.0);
+            this.sendDataToRegistry(INTERMODULE_VARIABLE.MOTOR_SPEED_RIGHT, baseSpeed);
+        }
+
+        private void hardRight()
+        {
+            this.sendDataToRegistry(INTERMODULE_VARIABLE.MOTOR_SPEED_LEFT, baseSpeed);
+            this.sendDataToRegistry(INTERMODULE_VARIABLE.MOTOR_SPEED_RIGHT, 0.0);
+        }
+
+        private void forward()
+        {
+            this.sendDataToRegistry(INTERMODULE_VARIABLE.MOTOR_SPEED_LEFT, baseSpeed);
+            this.sendDataToRegistry(INTERMODULE_VARIABLE.MOTOR_SPEED_RIGHT, baseSpeed);
+        }
+
+        public override void shutdown()
+        {
+            base.shutdown();
         }
     }
 }
